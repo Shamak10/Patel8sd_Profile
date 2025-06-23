a3cb9be97bbb1424f759de87a3c3f4c575e62234
Shamak10 <patel8sd@mail.uc.edu>
2025-06-23 12:12:12 -0400

    Add project files.

---
+++ Patel8sd_Profile/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Patel8sd_Profile
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
